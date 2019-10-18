b3c64063bddc717f32a3a70f07856f9039402a5b
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-18 11:30:54 +0100

    Add user form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 
 
 class Form extends Component {
+  constructor(){
+    super();
+    this.state = {
+      value: '',
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
   render() {
     return (
       <div>
-        <form>
-          <label>
-            Name:
-    <input type="text" name="name" />
+        <form onSubmit={this.handleSubmit} >
+          <label>First Name: 
+          <input type="text" 
+                 name="fname"   
+                 value={this.state.value} 
+                 onChange={this.handleChange} 
+          />
           </label>
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit"/>
         </form>
+
+        <div>{this.state.value}</div>
       </div>
     )
   }
