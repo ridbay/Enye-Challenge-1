30205a21defc35d56d100cc670fe296ee7b99040
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-21 15:25:46 +0100

    Updates

---
+++ precache-manifest.1504c507af3c51094600b0c30e7c7804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfe632ea2069c9dbe0550a8c51cb27f",
+    "url": "/Enye-Challenge-1/index.html"
+  },
+  {
+    "revision": "3d1c136929ce491bd3fe",
+    "url": "/Enye-Challenge-1/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "2f6b405044050c815dd0",
+    "url": "/Enye-Challenge-1/static/css/main.7089d92d.chunk.css"
+  },
+  {
+    "revision": "3d1c136929ce491bd3fe",
+    "url": "/Enye-Challenge-1/static/js/2.06fafcf0.chunk.js"
+  },
+  {
+    "revision": "2f6b405044050c815dd0",
+    "url": "/Enye-Challenge-1/static/js/main.8e0be090.chunk.js"
+  },
+  {
+    "revision": "5963914da2b997ba2c95",
+    "url": "/Enye-Challenge-1/static/js/runtime-main.6bc271fd.js"
+  }
+]);
