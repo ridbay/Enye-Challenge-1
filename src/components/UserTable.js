548c8229eac65a5b173b0529a2b557dec7336989
Ridwan Balogun <balogunridwan@gmail.com>
2019-10-31 18:08:01 +0100

    'update'

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,34 +1,34 @@
 import React from 'react';
 // import './Table.css';
 
-const UserList = ({users})=> {
+const UserList = ({ users }) => {
 
-        return (
-            <div>
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Birthday</th>
-                            <th>Age (years)</th>
-                            <th>Hobby</th>
-                        </tr>
-                        {users.map(user=> {
-                            return (
-                                <tr key={user.id}>
-                                    <td>{user.firstName}</td>
-                                    <td>{user.lastName}</td>
-                                    <td>{user.birthday.birthday}</td>
-                                    <td>{user.age.age}</td>
-                                    <td>{user.hobby}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        )
+    return (
+        <div>
+            <table className="table">
+                <tbody>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Birthday</th>
+                        <th>Age (years)</th>
+                        <th>Hobby</th>
+                    </tr>
+                    {users.map(user => {
+                        return (
+                            <tr key={user.id}>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.birthday.birthday}</td>
+                                <td>{user.age.age}</td>
+                                <td>{user.hobby}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 
